91fbb1008bf4adbb002857a42ce3ae23cdbee3a2
Shawn Aviles <shawnaviles26@gmail.com>
2024-10-27 04:07:10 -0400

    feat: created user statistic page

--- client/constants/Colors.ts
+++ client/constants/Colors.ts
@@ -4,13 +4,20 @@
  */
 
 const tintColorLight = '#ff9924';
-const tintColorDark = '#fff';
 const primaryColor = "#88CBFF";
 const accentColor = "#FF9924";
 const secondaryColor = "#E1EFFF"
 const shadowColor = "#AAB3BF";
 const white = "#FFFFFF";
 const black = "#000000";
+const gray = "#444444";
+
+// Pastel colors for topics
+const shortTermMemoryColor = '#FF8A8A'; // Darker pastel color for short-term memory
+const concentrationColor = '#8AFF8A'; // Darker pastel color for concentration
+const problemSolvingColor = '#8AB3FF'; // Darker pastel color for problem-solving
+const numericalReasoningColor = '#FFB38A'; // Darker pastel color for numerical reasoning
+const visualSpatialColor = '#B38AFF'; // Darker pastel color for visual-spatial
 
 export const Colors = {
   light: {
@@ -34,5 +41,11 @@ export const Colors = {
   secondary: secondaryColor,
   shadow: shadowColor,
   white: white,
-  black: black
+  black: black,
+  gray: gray,
+  shortTermMemory: shortTermMemoryColor,
+  concentration: concentrationColor,
+  problemSolving: problemSolvingColor,
+  numericalReasoning: numericalReasoningColor,
+  visualSpatial: visualSpatialColor,
 };
