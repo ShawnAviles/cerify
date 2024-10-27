2a334a4814157f9d84276d29d655ae780b129e83
Christian Apostol <christiand.apostol@outlook.com>
2024-10-27 05:38:43 -0400

    new schema

--- backend/models/User.js
+++ backend/models/User.js
@@ -22,31 +22,55 @@ const UserSchema = new mongoose.Schema(
       type: String,
       required: true,
     },
-    caregiver: {
-      firstName: {
-        type: String,
-        required: true,
-      },
-      lastName: {
-        type: String,
-        required: true,
-      },
-      email: {
-        type: String,
-        required: true,
-      },
+    caregiverID: {
+      type: String,
+      required: false,
     },
-    scores: {
-      matching: {
-        firstScore: Number,
-        scoreHistory: [Number],
-        runningAverage: Number,
-        runningTotalAttempts: Number,
+    stats: {
+      exercisesCompleted: {
+        type: Number,
+        required: false,
       },
+      totalXp: {
+        type: Number,
+        required: false
+      },
+      memoryLevel: {
+        type: Number,
+        required: false,
+      }
+      // TODO: Other 3 stats tbd
     },
     goals: [String],
   },
   { timestamps: true }
 );
+
+const caregiverSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    firstName: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+    },
+    patients: [String],
+  },
+  { timestamps: true }
+)
 
 export default mongoose.model("User", UserSchema);
