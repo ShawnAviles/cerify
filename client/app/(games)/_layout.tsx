c5c2e2f4fefd9d0163c93cdda9a30e5d0a6625a2
j-ferber <ferberjustin5@gmail.com>
2024-10-27 02:33:56 -0400

    deleted extra comments

--- client/app/(games)/_layout.tsx
+++ client/app/(games)/_layout.tsx
@@ -37,6 +37,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: 'white', // Background behind navbar
-    // paddingBottom: insets.bottom,
   },
 });
--- client/app/(tabs)/_layout.tsx
+++ client/app/(tabs)/_layout.tsx
@@ -66,6 +66,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: 'white', // Background behind navbar
-    // paddingBottom: insets.bottom,
   },
 });
