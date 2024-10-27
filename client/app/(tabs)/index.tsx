86b4f99bb4d819184c9a308db93f8e85851d5c9e
Christian Apostol <christiand.apostol@outlook.com>
2024-10-27 11:16:54 -0400

    adding patients page

--- client/app/(tabs)/index.tsx
+++ client/app/(tabs)/index.tsx
@@ -4,16 +4,14 @@ import { Colors } from '@/constants/Colors';
 import { WelcomeHeader } from '@/components/WelcomeHeader';
 import { BrainExperienceBar } from '@/components/BrainExperienceBar';
 import { GameButtonLayout } from '@/components/GameButtonLayout';
-import Patients from '../(caregiver)/patients';
 
 export default function HomeScreen() {
   return (
-    <Patients />
-    // <ThemedView style={styles.page} lightColor={Colors.primary} darkColor={Colors.primary}>
-    //   <WelcomeHeader />
-    //   <BrainExperienceBar />
-    //   <GameButtonLayout />
-    // </ThemedView>
+    <ThemedView style={styles.page} lightColor={Colors.primary} darkColor={Colors.primary}>
+      <WelcomeHeader />
+      <BrainExperienceBar />
+      <GameButtonLayout />
+    </ThemedView>
   );
 }
 
