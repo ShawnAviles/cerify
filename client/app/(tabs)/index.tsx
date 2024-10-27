972a2d6f39cf2a402cac590ad00b73f426c39b53
Christian Apostol <christiand.apostol@outlook.com>
2024-10-27 04:01:31 -0400

    moved Matching game to router

--- client/app/(tabs)/index.tsx
+++ client/app/(tabs)/index.tsx
@@ -4,16 +4,14 @@ import { Colors } from '@/constants/Colors';
 import { WelcomeHeader } from '@/components/WelcomeHeader';
 import { BrainExperienceBar } from '@/components/BrainExperienceBar';
 import { GameButtonLayout } from '@/components/GameButtonLayout';
-import { Matching } from '@/components/views/Matching';
 
 export default function HomeScreen() {
   return (
-    <Matching />
-    // <ThemedView style={styles.page} lightColor={Colors.primary} darkColor={Colors.primary}>
-    //   <WelcomeHeader />
-    //   <BrainExperienceBar />
-    //   <GameButtonLayout />
-    // </ThemedView>
+    <ThemedView style={styles.page} lightColor={Colors.primary} darkColor={Colors.primary}>
+      <WelcomeHeader />
+      <BrainExperienceBar />
+      <GameButtonLayout />
+    </ThemedView>
   );
 }
 
