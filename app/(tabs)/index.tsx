d9126979d677e265c491ca944a03bc2c5e8b4e4a
Justin Ferber <119906373+j-ferber@users.noreply.github.com>
2024-10-26 20:39:35 -0400

    Almost finished home page (#3)

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,53 +1,91 @@
-import { Image, StyleSheet, Platform } from 'react-native';
-
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
+import { Image, StyleSheet} from 'react-native';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
 import {format} from 'date-fns';
+import CircularProgress from 'react-native-circular-progress-indicator';
 
 export default function HomeScreen() {
+
+  const value = 10 // This value should be fetched from the API
+
   return (
     <ThemedView lightColor='#88CBFF' darkColor='#88CBFF' style={styles.page}>
       <ThemedText type="default" style={styles.welcomeText}>Welcome, John!</ThemedText>
       <ThemedText type="default" style={styles.todayText}>Today is</ThemedText>
-      <ThemedText type="title">{format(new Date(), "eeee, MMMM dd")}</ThemedText>
-      <ThemedView lightColor='#88CBFF' darkColor='#88CBFF'>
-        <Image source={require("@/assets/images/brain.png")} style={styles.brainImage}/>
+      <ThemedText type="title" style={styles.dateText}>{format(new Date(), "eeee, MMMM dd")}</ThemedText>
+      <ThemedView lightColor='#88CBFF' darkColor='#88CBFF' style={styles.stepView}>
+        <Image source={require("@/assets/images/brain.png")} style={styles.brainImage} resizeMode='contain'/>
+        <CircularProgress 
+          value={value} 
+          radius={130} 
+          maxValue={100}
+          activeStrokeWidth={32} 
+          inActiveStrokeWidth={32}
+          inActiveStrokeColor='#F2F2F2'
+          activeStrokeColor='#FF9924'
+          showProgressValue={false}
+          clockwise={false}
+        />
+        <ThemedText type="title" lightColor='#FF9924' darkColor='#FF9924' style={styles.progressText}>{value} / 100 XP</ThemedText>
+      </ThemedView>
+      <ThemedView lightColor='#88CBFF' darkColor='#88CBFF' style={styles.bottomSplitView}>
+        <ThemedText type="title">Daily Goal</ThemedText>
+        <ThemedText type="default">Complete 5 lessons</ThemedText>
       </ThemedView>
     </ThemedView>
   );
 }
 
 const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
-  },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
-  },
   page: {
     flex: 1,
     paddingTop: 40,
-    paddingHorizontal: 20,
   },
   welcomeText: {
     marginBottom: 24,
+    fontWeight: 'bold',
+    paddingHorizontal: 24
   },
   todayText: {
     marginBottom: 8,
+    paddingHorizontal: 24
   }, 
+  dateText: {
+    paddingHorizontal: 24
+  },
   brainImage: {
-    width: 60,
+    width: "50%",
+    position: 'absolute',
+    top: '50%', // Center vertically
+    left: '50%', // Center horizontally
+    transform: [{ translateX: -100 }, { translateY: -130 }], 
+  },
+  stepView: {
+    display: 'flex',
+    position: 'relative',
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '100%',
+    marginVertical: 24
+  }, 
+  bottomSplitView: {
+    display: 'flex',
+    position: 'relative',
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '100%',
+    flex: 1,
+    backgroundColor: "#FFFFFF",
+    borderTopLeftRadius: 40,
+    borderTopRightRadius: 40,
+  }, 
+  progressText: {
+    textShadowColor: 'black',
+    textShadowOffset: { width: 0, height: 0 },
+    textShadowRadius: 8,
+    width: "100%",
+    textAlign: 'center',
+    marginTop: 8,
+    paddingVertical: 8,
   }
 });
