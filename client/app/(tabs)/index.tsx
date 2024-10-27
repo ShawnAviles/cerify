22e53dfbcfcc7f483abaa33334486e47e00a1d4f
Christian Apostol <christiand.apostol@outlook.com>
2024-10-26 23:41:22 -0400

    simplified index

--- client/app/(tabs)/index.tsx
+++ client/app/(tabs)/index.tsx
@@ -1,16 +1,5 @@
 import { StyleSheet } from 'react-native';
 import { ThemedView } from '@/components/ThemedView';
-import { GameButton } from '@/components/GameButton';
-import { GameButton } from '@/components/GameButton';
-import {format} from 'date-fns';
-import CircularProgress from 'react-native-circular-progress-indicator';
-import Login from '@/components/views/Login';
-import { SafeAreaView } from 'react-native-safe-area-context';
-import { Colors } from '@/constants/Colors';
-import { WelcomeHeader } from '@/components/WelcomeHeader';
-import { BrainExperienceBar } from '@/components/BrainExperienceBar';
-import { GameButtonLayout } from '@/components/GameButtonLayout';
-import { SafeAreaView } from 'react-native-safe-area-context';
 import { Colors } from '@/constants/Colors';
 import { WelcomeHeader } from '@/components/WelcomeHeader';
 import { BrainExperienceBar } from '@/components/BrainExperienceBar';
